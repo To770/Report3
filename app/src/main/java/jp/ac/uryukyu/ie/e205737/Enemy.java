779dad46a9fa62b58bb633035d18d9daaf09c0bf
Tomo AKAMINE <e205737@ie.u-ryukyu.ac.jp>
2021-01-24 15:14:58 +0900

    report3 step2

--- app/src/main/java/jp/ac/uryukyu/ie/e205737/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205737/Enemy.java
@@ -9,10 +9,18 @@ package jp.ac.uryukyu.ie.e205737;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean getDead(){
+        return this.dead;
+    }
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -25,7 +33,7 @@ public class Enemy {
         hitPoint = maximumHP;
         this.attack = attack;
         dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", getName(), maximumHP, attack);
     }
 
     /**
@@ -36,7 +44,7 @@ public class Enemy {
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
         if(this.dead == false){
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), hero.getName(), damage);
             hero.wounded(damage);
         }
     }
